a2cdc4aa3e97ec6e765310a4c27478bf4de8a117
KyleGilmartin <kylegilmartin8@gmail.com>
2022-04-07 19:12:23 +0100

    setting up retrofit

---
+++ app/src/main/java/com/example/stepfitfrontend/data/AuthenticationService.kt
@@ -0,0 +1,10 @@
+package com.example.stepfitfrontend.data
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthenticationService {
+
+    @POST("Account/Register")
+    suspend fun registerUser(@Body registerUser: RegisterUser)
+}
